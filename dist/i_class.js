f07aa92cb23cd4d3f3f01ce899258561921f5e60
Tandoh Anthony Nwi-Ackah <anthonytandoh90@yahoo.com>
2025-03-13 16:57:24 +0000

    commit access modifiers

--- dist/i_class.js
+++ dist/i_class.js
@@ -4,10 +4,10 @@
 //-------------
 class Pizza {
     constructor(title, price) {
-        this.base = "classic";
-        this.toppings = [];
         this.title = title;
         this.price = price;
+        this.base = "classic";
+        this.toppings = [];
     }
     addTopping(topping) {
         this.toppings.push(topping);
